fc256106fd573ba73d756c862e6e31d654a7d9e0
Frederick Ivan <pigifier2000@gmail.com>
2023-11-20 13:34:44 +0700

    menambah template ke trainer page

--- app/trainer/page.tsx
+++ app/trainer/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+'use client';
 
-const page = () => {
+import styles from './login.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
+import Navbar from '@/components/Navbar'
+
+const Hero = () => {
+    const handleScroll = () => {
+
+    }
   return (
-    <div>page</div>
+    <div className='hero'>
+        <Navbar />
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+                GetFitt
+            </h1>
+            
+            <p className='hero__subtitle'>
+                Get fit and healthy!
+            </p>
+        </div>
+        <div className='hero__image-container'>
+            <div className='hero__image'>
+                <Image src='/bodybuilder.png' alt='hero' fill className='object-contain'  />
+            </div>
+        </div>
+    </div>
   )
 }
 
-export default page
+export default Hero
--- app/trainerprofile/page.tsx
+++ app/trainerprofile/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Image from "next/image";
+import face from "../public/face.png";
 
-const page = () => {
+function Card() {
   return (
-    <div>page</div>
+    <div className='cursor-pointer m-1 rounded-2xl flex flex-col justify-start items-center w-96 border-8 border-gradient-to-l from-blue-300 via-white to-blue-300 bg-gradient-to-tr h-3/4'>
+          <div>
+              <div className='flex justify-center items-center py-4'>
+              <Image src={face} width="90" height="90" className='rounded-lg sepia ' alt={''}/>
+              </div>
+              <h1 className='text-neutral-700 font-bold text-3xl text-center'>Orang Ngegym</h1>
+              <p className='font-normal uppercase text-base text-neutral-700 text-center pb-4'>Trainee</p>
+              <h3 className='font-semibold text-md uppercase text-center py-2'>About</h3>
+              <p className='text-sm text-neutral-600 text-center mx-10'>Saya pengen nurun berat badan dan menguatkan otot</p>
+              <div className='flex justify-center items-center gap-6 py-6'>
+              </div>
+              <div className='flex justify-center items-center gap-6 py-6'>
+              <button className='text-white uppercase bg-gradient-to-r hover:bg-gradient-to-l from-cyan-500 to-blue-500 p-3 font-semibold rounded-lg w-11/12 '>Mari Bekerja</button>
+              </div>
+             </div>      
+    </div>
   )
 }
 
-export default page
+export default Card
